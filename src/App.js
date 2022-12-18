c5d52718aaa226a939af68588f3469295d96b0aa
Adam <lll-s-lll@mail.ru>
2022-12-18 13:58:53 +0300

    add signs, numbers, firstValue and secondValue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [firstValue, setFirstValue] = React.useState(0);
+  const [secondValue, setSecondValue] = React.useState(0);
+  const [sign, setSign] = React.useState('');
+
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const mathSigns = ['+', '-', '/', '*'];
+
+  const onClickNumber = (number) => {
+    if (sign === '') {
+      setFirstValue(firstValue * 10 + number);
+    } else {
+      setSecondValue(secondValue * 10 + number);
+    }
+  };
+  const onClickSign = (sign) => {
+    setSign(sign);
+  };
+  console.log(firstValue);
+  console.log(secondValue);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{firstValue}</div>
+      <div>{secondValue}</div>
+      {numbers.map((n, i) => (
+        <div onClick={() => onClickNumber(i)}>{n}</div>
+      ))}
+      {mathSigns.map((s) => (
+        <div onClick={() => onClickSign(s)}>{s}</div>
+      ))}
     </div>
   );
 }
