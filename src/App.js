3e91fd04fdd2d8f8a0d6c3dabd5c5ccc54466245
Adam <lll-s-lll@mail.ru>
2022-12-18 15:08:39 +0300

    add thirdValues, equals. Added basic mechanics

--- src/App.js
+++ src/App.js
@@ -3,34 +3,62 @@ import './App.css';
 
 function App() {
   const [firstValue, setFirstValue] = React.useState(0);
-  const [secondValue, setSecondValue] = React.useState(0);
-  const [sign, setSign] = React.useState('');
+  const [secondValue, setSecondValue] = React.useState(null);
+  const [thirdValue, setThirdValue] = React.useState(null);
+  const [sign, setSign] = React.useState(null);
 
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const mathSigns = ['+', '-', '/', '*'];
+  const equals = '=';
 
   const onClickNumber = (number) => {
-    if (sign === '') {
+    setThirdValue(null);
+    if (sign === null) {
       setFirstValue(firstValue * 10 + number);
     } else {
       setSecondValue(secondValue * 10 + number);
     }
   };
+
   const onClickSign = (sign) => {
-    setSign(sign);
+    if (firstValue == null) {
+      setFirstValue(thirdValue);
+      setThirdValue(null);
+      setSign(sign);
+    } else {
+      setSign(sign);
+    }
+  };
+
+  const onClickEquals = () => {
+    if (sign === '+') {
+      setThirdValue(firstValue + secondValue);
+    } else if (sign === '-') {
+      setThirdValue(firstValue - secondValue);
+    } else if (sign === '/') {
+      setThirdValue(firstValue / secondValue);
+    } else if (sign === '*') {
+      setThirdValue(firstValue * secondValue);
+    }
+    setFirstValue(null);
+    setSecondValue(null);
+    setSign(null);
   };
   console.log(firstValue);
   console.log(secondValue);
   return (
     <div className="App">
       <div>{firstValue}</div>
+      <div>{sign}</div>
       <div>{secondValue}</div>
+      <div>{thirdValue}</div>
       {numbers.map((n, i) => (
-        <div onClick={() => onClickNumber(i)}>{n}</div>
+        <button onClick={() => onClickNumber(i)}>{n}</button>
       ))}
       {mathSigns.map((s) => (
-        <div onClick={() => onClickSign(s)}>{s}</div>
+        <button onClick={() => onClickSign(s)}>{s}</button>
       ))}
+      <button onClick={onClickEquals}>{equals}</button>
     </div>
   );
 }
